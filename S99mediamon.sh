db363b2cef30e64a2fa72119ac406bbf36bd8a4f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-11-01 09:29:44 +0100

    Add the status message

--- S99mediamon.sh
+++ S99mediamon.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 # /usr/syno/etc/rc.d/S99mediamon.sh
 
+PIDFILE=/var/run/mediamon.pid
+
 case "$1" in
   start|"")
     #start the monitoring daemon
-    python /root/bin/mediamon.py
+    python /usr/local/bin/mediamon.py
     ;;
   restart|reload|force-reload)
-    echo "Error: argument '$1' not supported" >&2
-    exit 3
+   echo "Error: argument '$1' not supported" >&2
+   exit 3
+   ;;
+  status)
+    [ -f $PIDFILE ] && echo "mediamon is running with pid `cat $PIDFILE`" || echo "mediamon not running."
+    exit 0
     ;;
   stop)
-    kill `cat /var/run/mediamon.pid`
+    kill `cat $PIDFILE`
     ;;
   *)
-    echo "Usage: S99mediamon.sh [start|stop]" >&2
+    echo "Usage: S99mediamon.sh [start|stop|status]" >&2
     exit 3
     ;;
 esac
